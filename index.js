08f4245c797844d81299cd87748b081131ef80e7
fareehaabbasi <abbasifareeha9@gmail.com>
2025-03-11 10:07:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+//percentage calculator
+
+// let totalMarks = +prompt("How much total marks?");
+// let obtainedMarks = +prompt("How much obtained marks?");
+
+// let percentage = (totalMarks / obtainedMarks) * 100;
+
+// if(percentage >= 90) {
+//     document.write("Congratulation, your got 90%. You passed in first division")
+// }else if (percentage >= 80){
+//     document.write("Congratulation, your got 90%. You passed in second division")
+// }else if (percentage >= 70){
+//     document.write("Congratulation, your got 90%. You passed in second division")
+// }else if (percentage >= 60){
+//     document.write("Congratulation, your got 90%. You passed in second division")
+// }else if (percentage >= 50){
+//     document.write("Congratulation, your got 90%. You passed in second division")
+// }else if (percentage >= 40){
+//     document.write("Congratulation, your got 90%. You passed in third division")
+// }else if (percentage >= 35){
+//     document.write("Congratulation, your got 90%. You passed in third division")
+// }else{
+//     document.write("Sorry You're FAIL, Please try again")
+// }
+
+
+//User authentication 
+
+// let fullName = "Fareeha abbasi"
+// let password = "abbasifareeha"
+
+// let name1 = prompt("Enter your full name");
+// let email = prompt("Enter your email");
+
+// if(name1 === fullName || email === password) {
+//     document.write ("Your login in your account");
+// }else {
+//     document.write("Something Went wrong")
+// }
+
+
+//Order List
+
+let shoes = "shoes";
+let assesories = "assesories";
+let clothes = "Clothes";
+let makeup = "Makeup";
+
+document.write("<ol>" + "<li>" + shoes + "</li>" + "<li>" + clothes + "</li>" +  "<li>" + makeup + "</li>"  + "<li>" + assesories + "</li>" + "</ol>"); 
